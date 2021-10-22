a0cccce908198f598bfa461fc52f2384f26255c5
Kyriios188 <36565218+Kyriios188@users.noreply.github.com>
2021-10-22 09:06:57 +0200

    Update Principal.c

--- projet/Source/Principal.c
+++ projet/Source/Principal.c
@@ -12,14 +12,27 @@ void Callback (void) {
 
 int main(void) {	
 	
+MyEncoder_Struct_TypeDef Encoder;
+
+	MyGPIO_Struct_TypeDef GPIO_I = {GPIOA, 4, IN_PULLUP}; // configuration pas 100% certain
+	MyTimer_Struct_TypeDef compteur_AB = {TIM2, 720, 1};
+	
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_TIM1EN ;	
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN ;
+	
+	
+	Encoder.Encoder_I=&GPIO_I;
+	Encoder.struct_compteur_AB=&compteur_AB;
+	MyEncoder_Init(&Encoder);
+	
+	MyEncoder_findZero();
+
+
+
+
 	MyTimer_Struct_TypeDef TIMER3 ;
 	
 	MyGPIO_Struct_TypeDef BROCHE_CONFIG_PWM ;
-	
-	
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_TIM1EN ;
-	
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN ;
 	
 	TIMER3.ARR = 60000 ;
 	TIMER3.PSC = 24 ;
