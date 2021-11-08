af5a9eca6d6633094cb442cb3b82c948ce11e7fd
Matthias <75242607+moutth@users.noreply.github.com>
2021-11-08 10:27:55 +0100

    Update Bordage.h
    correct merge

--- projet/Include/Bordage.h
+++ projet/Include/Bordage.h
@@ -7,15 +7,10 @@
 #include "Driver_Servo.h"
 #include "MyEncoder.h"
 
-<<<<<<< HEAD
 //expliquer choix ici
 #define INTERRUPT_SERVO_ARR 120
 #define INTERRUPT_SERVO_PSC 60000
-//mettre ça dans un .c propre
-=======
-#define INTERRUPT_SERVO_ARR 120
-#define INTERRUPT_SERVO_PSC 60000
->>>>>>> 2e6684cc66243fe25b01975e259562898acbdc3a
+//mettre Ã§a dans un .h propre
 #define PRIO_INTERRUPT_BORDAGE 4
 
 void Bordage_Init(MyTimer_Struct_TypeDef * timer, MyGPIO_Struct_TypeDef * pin_servo, char channel_pwm, MyEncoder_Struct_TypeDef * encoder);
