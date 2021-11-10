e518f5c0d163eeb451a88f00eee8651b9ac589fe
Valentin Guerbette <guerbett@insat.insa-toulouse.fr>
2021-11-10 13:25:15 +0100

    Enum

--- projet/Source/MyUART.c
+++ projet/Source/MyUART.c
@@ -1,51 +1,50 @@
 #include "MyUART.h"
 #include "stm32f10x.h"
 
-void MyUART_Init(USART_TypeDef * USART, float Baud_Rate){
+void MyUART_Init(USART_TypeDef * USART, USART_Mode_TypeDef Mode, float Baud_Rate){
 	
+	USART->BRR = FREQ_CPU / Baud_Rate ; //baud USART3  = = = 4800?????
+	
+	if(Mode == RX){
+		// Enable Receiver
+		USART->CR1 |= USART_CR1_RE;
+
+		// Enable interrupt on RXNE event
+		USART->CR1 |= USART_CR1_RXNEIE;
+		
+		// Enable USART
+		USART->CR1 |= USART_CR1_UE;
+
+		switch((int)USART) {
+			case (int)(USART1) :
+	//			// Enable USART clock
+	
+				NVIC_SetPriority(USART1_IRQn, 1);
+				NVIC_EnableIRQ(USART1_IRQn);
+				break;
+			
+			case (int)(USART2) : 
+
 				
-	
-	
-	// Enable both Transmitter and Receiver
-	USART->CR1 |= USART_CR1_RE | USART_CR1_TE;
 
-	// Enable interrupt on RXNE event
-	USART->CR1 |= USART_CR1_RXNEIE;
-	
-	// Enable USART
-	USART->CR1 |= USART_CR1_UE;
-	USART->BRR = FREQ_CPU / Baud_Rate ; 
-	
-	//RDR : Interrupt init
-	//USART->CR1 |= USART_CR1_RXNEIE ;
-	
-	switch((int)USART) {
-		case (int)(USART1) :
-//			
-//			// Enable USART clock
-//			RCC -> APB2ENR |= RCC_APB2ENR_USART1EN;
-
-			NVIC_SetPriority(USART1_IRQn, 1);
-			NVIC_EnableIRQ(USART1_IRQn);
-			break;
-		
-		case (int)(USART2) : 
-
+				NVIC_SetPriority(USART2_IRQn, 1);
+				
+				NVIC_EnableIRQ(USART2_IRQn);
+				break;
 			
-
-			NVIC_SetPriority(USART2_IRQn, 1);
-			
-			NVIC_EnableIRQ(USART2_IRQn);
-			break;
-		
-		case (int)(USART3) : 
-			
-			NVIC_SetPriority(USART3_IRQn, 1);
-			
-			NVIC_EnableIRQ(USART3_IRQn);
-			break;	
+			case (int)(USART3) : 
+				
+				NVIC_SetPriority(USART3_IRQn, 1);
+				
+				NVIC_EnableIRQ(USART3_IRQn);
+				break;	
+		}
 	}
 	
+	if(Mode == TX){
+		// Enable Transmiter
+		USART->CR1 |= USART_CR1_TE ;
+	}
 }
 int n = 0;
 char comp = 0;
--- projet/Tests/TestUSART.c
+++ projet/Tests/TestUSART.c
@@ -2,7 +2,7 @@
 #include "Driver_GPIO.h"
 
 MyGPIO_Struct_TypeDef GPIO_USART_RX = {GPIOA,10,ALTOUT_PPULL};
-MyGPIO_Struct_TypeDef GPIO_USART_TX = {GPIOB,10,IN_PULLDOWN};
+//MyGPIO_Struct_TypeDef GPIO_USART_TX = {GPIOB,10,IN_PULLDOWN};
 
 char test_string[] = "coucou" ; 
 int main(){
@@ -10,19 +10,19 @@ int main(){
 	RCC -> APB2ENR |= RCC_APB2ENR_USART1EN | RCC_APB2ENR_IOPAEN;
 	
 	// Enable USART2 clock
-	RCC -> APB1ENR |= RCC_APB1ENR_USART2EN;
+	//RCC -> APB1ENR |= RCC_APB1ENR_USART2EN | RCC_APB2ENR_IOPBEN;
 	
-	// Enable USART clock
-	RCC -> APB1ENR |= RCC_APB1ENR_USART3EN;
+	// Enable USART3 clock
+	//RCC -> APB1ENR |= RCC_APB1ENR_USART3EN;
 
 	MyGPIO_Init(&GPIO_USART_RX);
-	MyGPIO_Init(&GPIO_USART_TX);
+	//MyGPIO_Init(&GPIO_USART_TX);
 	
-	MyUART_Init(USART1, 9600);
-	MyUART_Init(USART3, 9600);
+	MyUART_Init(USART1, RX, 9600);
+	//MyUART_Init(USART3, TX, 9600);
 	
 	while(1){
-		MyUART_Send(USART3,test_string,7);
+		//MyUART_Send(USART3,test_string,7);
 	}
 	return 0;
 }
