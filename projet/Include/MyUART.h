7648f69e6ae3a403da5d80711ab9dd042ad8752a
Valentin Guerbette <guerbett@insat.insa-toulouse.fr>
2021-11-10 13:28:04 +0100

    Enum

--- projet/Include/MyUART.h
+++ projet/Include/MyUART.h
@@ -3,13 +3,23 @@
 
 #include "stm32f10x.h"
 
-//Inits interrupt (reception) and starts background process (emission)
-void MyUART_Init(USART_TypeDef * USART, short Baud_Rate);
+#ifndef FREQ_CPU
+#define FREQ_CPU 72000000.
+#endif
 
-//Sends a string
-void MyUART_Send(char * M);
+//typedef enum{
+//	RX,TX
+//}USART_Mode_TypeDef ; 
+
+//Inits interrupt (reception) or sets USART mode to transmission
+// TX mode is to be used via pooling method 
+// RX mode is based on RXNEI flag interruption : to retrieve received byte, handlers have to be redefined
+void MyUART_Init(USART_TypeDef * USART, float Baud_Rate);
+
+//Sends a string M with length len
+void MyUART_Send(USART_TypeDef * USART, char * M, int len);
 
 //Receives a string
-void MyUART_Receive(char * M);
+void MyUART_Receive(USART_TypeDef * USART, char * M);
 
 #endif
