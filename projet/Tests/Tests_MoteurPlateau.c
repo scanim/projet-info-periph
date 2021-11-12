4cf34072d4dca9ffc35ae21b564526e72e18409d
Matthias <75242607+moutth@users.noreply.github.com>
2021-11-12 17:39:13 +0100

    Update Tests_MoteurPlateau.c
    Sauvé !

--- projet/Tests/Tests_MoteurPlateau.c
+++ projet/Tests/Tests_MoteurPlateau.c
@@ -6,8 +6,8 @@
 #include "MyUART.h"
 
 
-void SpeedUpdate (USART_TypeDef usart) {
-	int valeur = usart.DR ;
+void SpeedUpdate () {
+	int valeur = USART1->DR ;
 	if (valeur>=0) {
 		MyGPIO_Set (GPIOB, 5) ;
 		PWM_Duty_Cycle (TIM4, (double)(valeur)/100.0, '1') ;
@@ -17,7 +17,6 @@ void SpeedUpdate (USART_TypeDef usart) {
 	}
 	
 }
-
 int main(void) {	
 	
 	/*int i ;
@@ -47,10 +46,10 @@ int main(void) {
 	MyGPIO_Init (&BROCHE_PWM) ;
 	MyGPIO_Init (&PIN_SENS) ;
 	MyGPIO_Init(&GPIO_USART_RX);
-	MyUART_Init(USART1, RX, 9600, SpeedUpdate);
-	
+	MyUART_Init(USART1, RX, 9600);
+
 	MyTimer_PWM (TIMER4.Timer, '1') ;
-	
+	MyUART_RX_ActiveIT(USART1, SpeedUpdate);
 	
 	
 	
@@ -61,8 +60,8 @@ int main(void) {
 	
 	
 	
-	// PWM_Duty_Cycle (TIMER4.Timer, 20, '2') ; À CHAQUE INTERRUPTION
-	// MISE À JOUR DU RAPPORT CYCLIQUE + ACTIVATION BIT DE SENS
+	// PWM_Duty_Cycle (TIMER4.Timer, 20, '2') ; Ã€ CHAQUE INTERRUPTION
+	// MISE Ã€ JOUR DU RAPPORT CYCLIQUE + ACTIVATION BIT DE SENS
 	
 	
 	
