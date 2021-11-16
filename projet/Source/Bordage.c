2d78824990ba6564dbe1a872a90f28bc25a74e04
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-11-16 16:41:49 +0100

    Update Bordage.c

--- projet/Source/Bordage.c
+++ projet/Source/Bordage.c
@@ -9,7 +9,7 @@
 // Variables globales qui seront utilisees dans le handler (on ne peut pas lui passer d'argument)
 MyTimer_Struct_TypeDef * TIMER_PWM_SERVO;
 MyGPIO_Struct_TypeDef * PIN_SERVO;
-char * CHANNEL_PWM_SERVO;
+char CHANNEL_PWM_SERVO;
 MyEncoder_Struct_TypeDef * ENCODER;
 
 void Bordage_Init(MyTimer_Struct_TypeDef * timer, MyGPIO_Struct_TypeDef * pin_servo, char channel_pwm, MyEncoder_Struct_TypeDef * encoder){
@@ -25,17 +25,17 @@ void Bordage_Init(MyTimer_Struct_TypeDef * timer, MyGPIO_Struct_TypeDef * pin_se
 	// Initialisation des variables du handler
 	TIMER_PWM_SERVO = timer;
 	PIN_SERVO = pin_servo;
-	*CHANNEL_PWM_SERVO = channel_pwm;
+	CHANNEL_PWM_SERVO = channel_pwm;
 	ENCODER = encoder;
 }
 
 extern void Handler_Bordage(){
 	int mesure = (int)MyEncoder_getPosition(ENCODER);
 		if ((0 <= mesure && mesure < 45) || (315 <= mesure && mesure < 360)){
-			Servo_Set_Angle(*TIMER_PWM_SERVO, 0, *CHANNEL_PWM_SERVO);
+			Servo_Set_Angle(*TIMER_PWM_SERVO, 0, CHANNEL_PWM_SERVO);
 		} else if (45 <= mesure && mesure < 180) {
-			Servo_Set_Angle(*TIMER_PWM_SERVO, 0.6666666667*(mesure-45), *CHANNEL_PWM_SERVO);
+			Servo_Set_Angle(*TIMER_PWM_SERVO, 0.6666666667*(mesure-45), CHANNEL_PWM_SERVO);
 		} else if (180 <= mesure && mesure < 315) {
-			Servo_Set_Angle(*TIMER_PWM_SERVO, 0.6666666667*(315-mesure), *CHANNEL_PWM_SERVO);
+			Servo_Set_Angle(*TIMER_PWM_SERVO, 0.6666666667*(315-mesure), CHANNEL_PWM_SERVO);
 		}
 }
