1be60116aafa82612fd2b17f777161b79dcda346
Valentin Guerbette <guerbett@insat.insa-toulouse.fr>
2021-11-17 12:10:00 +0100

    ADC

--- projet/Include/MyADC.h
+++ projet/Include/MyADC.h
@@ -1,5 +1,10 @@
 #ifndef MY_ADC
 #define MY_ADC
 
-//Proto . .. . 
+#include "stm32f10x.h"
+
+void MyADC_Init(ADC_TypeDef * ADC);
+void MyADC_ActiveIT( ADC_TypeDef * ADC, void (*IT_function) (void) ) ;
+void MyADC_ToggleConvert(ADC_TypeDef * ADC);
+int MyADC_Read(ADC_TypeDef * ADC);
 #endif
--- projet/Source/MyADC.c
+++ projet/Source/MyADC.c
@@ -1,3 +1,26 @@
- #include "MyADC.h"
+#include "MyADC.h"
+
+void (*pHandler_ADC) (void) ;
+
+void MyADC_Init(ADC_TypeDef * ADC){
+	ADC -> CR2 &= 0 ;
+	ADC -> CR2 |= ADC_CR2_ADON ; 
+}
+
+void MyADC_ActiveIT( ADC_TypeDef * ADC, void (*IT_function) (void) ){
+	ADC -> CR1 |= ADC_CR1_EOCIE ; 
+	pHandler_ADC = IT_function ;
+	NVIC_SetPriority(ADC1_2_IRQn, 3);
+	NVIC_EnableIRQ(ADC1_2_IRQn);
+}
+
+void MyADC_ToggleConvert(ADC_TypeDef * ADC){
+	ADC -> CR2 ^= ADC_CR2_SWSTART ; 
+}
+
+
+
+
+
  
  
